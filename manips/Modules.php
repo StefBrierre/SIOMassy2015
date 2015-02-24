937481687df5b63ce3f3dfa0d3cb8a93dbd555a5
Marion <mayondes@hotmail.fr>
2015-02-24 16:29:14 +0100

    Ajout Modules.php au projet

---
+++ manips/Modules.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>modules</title>
+    </head>
+    <body>
+        <p>Bonjour !!!</p>
+    </body>
+</html>
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
