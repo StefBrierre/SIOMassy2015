57ce9f7047841b8c60810eeff8e1ce9b50846c5d
Marion <mayondes@hotmail.fr>
2015-02-24 15:55:29 +0100

    Merge origin/master
    Conflicts:
    	manips/index.php

--- manips/index.php
+++ manips/index.php
@@ -1,10 +1,20 @@
-<?php
-?>
-<ul>
-  <li>Michel</li>
-  <li>Joel</li>
-  <li>Marion</li>
-  <li>Stéphanie</li>
-</ul>
-<p>Bonjour, comment ça va ?</p>
+<html>
+    <head>
+        <title>MaPage</title>
+    </head>
+    <body>
+        <input type="button" name="modules" value="Module1"/>
+        <input type="button" name="modules" value="Module2"/>
+        <input type="button" name="modules" value="Module3"/>
+        <input type="button" name="modules" value="Module4"/>
+        <input type="button" name="modules" value="Module5"/>
+        <input type="button" name="modules" value="Module6"/>
+        <input type="button" name="modules" value="Module7"/>
+    </body>
+</html>
 
+
+<?php
+
+
+?>
