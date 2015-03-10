d0fb45d0e2f73fda4af27539a25d3b566c7be181
Stéphanie <Stphanie@172.17.16.16>
2015-03-10 10:42:46 +0100

    Ajout JS et CSS pour le DatePicker + modification pour integration datepicker à CreerEval

--- CreerEval.php
+++ CreerEval.php
@@ -1,6 +1,16 @@
 <html>
     <head>
+        
         <title>Création d'une évaluation</title>
+        
+        <link rel = "stylesheet" type = "text/css" href = "items/css/BeatPicker.min.css"/>
+        
+        <!--  <script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.js"></script> -->
+        
+        <script type="text/javascript" src = "items/js/jquery.1.8.3.js"></script>
+		
+        <script type = "text/javascript" src = "items/js/BeatPicker.min.js"></script>
+        
     </head>
     <body>
         <h1>Création d'une évaluation</h1>
@@ -44,7 +54,15 @@
             <div class="row">
 
                 <label> Date de l'évaluation</label>
-                <input type="date">
+                
+                <input type="text" name="dateevaluation" placeholder="Date de l'évaluation" data-beatpicker="true" data-beatpicker-position="['10','50']"  data-beatpicker-extra="customOptions" data-beatpicker-format="['YYYY','MM','DD'],separator:'/'" data-beatpicker-module="icon">
+                
+                <script type="text/javascript">
+			customOptions = {
+				monthsFull : ["Janvier", "F\351vrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Ao\373t", "Septembre", "Octobre", "Novembre", "D\351cembre"],
+				daysSimple : ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa"],
+			}
+		</script>
 
             </div>
 
