ef68a4855129d1013dfc7830f536a9aa3037a9e8
Stéphanie <Stphanie@MoaPC>
2015-03-24 22:08:49 +0100

    Modifications sur la création du bilan

--- application/controllers/Creation_bilan.php
+++ application/controllers/Creation_bilan.php
@@ -4,13 +4,38 @@ class Creation_bilan extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->model('Creation_bilan_m');
+        $this->load->model('Menus_m');
+        $this->load->model('bilan_m');
     }
 
     public function index() {
-        $this->load->helper(array('form', 'url'));
-        $data["sessions"] = $this->Creation_bilan_m->getSessions();
-        $this->load->view('Creation_bilan_v', $data);
+        if ($this->input->server('REQUEST_METHOD') == "GET") {
+            $this->load->helper(array('form', 'url'));
+            $data["sessions"] = $this->Menus_m->getSessionsEnCours();
+            $this->load->view('Creation_bilan_v', $data);
+        } else {
+            // valider les données
+            // Charge les outils pour les formulaires
+            $this->load->helper('form');
+            $this->load->library('form_validation');
+            // Etablit les règles
+            $this->form_validation->set_rules('sessions', 'ID session', 'required|integer');
+            $this->form_validation->set_rules('datebilan', 'date', 'required');
+            // Modifie la forme des messages d'erreur
+            $this->form_validation->set_error_delimiters('<span class="error">', '</span>');
+
+            if (!$this->form_validation->run()) {
+                // Il y a des erreurs => réafficher le formulaire
+                $this->load->helper(array('form', 'url'));
+                $data["sessions"] = $this->Menus_m->getSessionsEnCours();
+                $this->load->view('Creation_bilan_v', $data);
+            } else {
+                // Pas d'erreurs => suite du traitement
+                $data['msg'] = "Choix enregistré";
+                $this->load->view('Creation_bilan_v', $data);
+                // si ok, inserer en base
+            }
+        }
     }
 
 }
---
+++ application/models/bilan_m.php
@@ -0,0 +1,15 @@
+<?php
+
+class bilan_m extends CI_Model {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
+
+    public function insert() {
+        
+    }
+
+}
