91a426d25360db02814b9b6ab6f0d2ec6c58133d
Michel Plasse <mplasse@free.fr>
2015-03-24 14:59:49 +0100

    Ajoute menus_m, et fait un test unitaire test_menus_m

---
+++ application/models/Menus_m.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Modele fournissant les données pour des listes déroulantes
+ */
+class Menus_m extends CI_Model {
+
+  /** Fonction de base fournissant une liste de données
+   * sous forme d'un tableau associatif value => text
+   * (ex : (1 => "BTS SIO", 2 => "BTS IRIS")
+   */
+  protected function getMap($sql) {
+    $query = $this->db->query($sql);
+    $rows = $query->result_array();
+    $result = array();
+    foreach ($rows as $row) {
+      $result[$row["value"]] = $row["text"];
+    }
+    return $result;
+  }
+
+    /** Sessions en cours sous forme d'un tableau associatif value => nom.
+   * PAS TERMINE : prend toutes les sessions !
+   */
+  public function getSessionsEnCours() {
+    $sql = "SELECT id_session AS value, nom AS text FROM session";
+    return $this->getMap($sql);
+  }
+}
